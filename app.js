41e745f990d3ec88c864a501650c3ac51c4adda2
Hoang Huu Giap <giaphh@inter-k.com>
2023-03-10 15:19:31 +0700

    initial commit

--- app.js
+++ app.js
@@ -3,6 +3,22 @@
 // const zoneIdList = require('./zoneIdList');
 import { zoneIdList } from './zoneIdList.js';
 import moment from 'moment';
+import cityTimezones from 'city-timezones';
+import {mst} from './microsoft_timezone.js';
+
+mst.forEach((city, index) => {
+  const result = city.split(',');
+  let timezoneValue = 'Nothing';
+  for (let item = 0; item < result.length; item++) {
+    const timezone = cityTimezones.lookupViaCity(result[item].trim());
+    if (timezone?.[0]?.timezone) {
+      timezoneValue = timezone?.[0]?.timezone;
+      break;
+    }
+    
+  }
+  console.log(`${index + 1}. ${city} - ${timezoneValue}`);
+})
 // import data from './data.json';
 
 // const result = [];
@@ -18,15 +34,15 @@ import moment from 'moment';
 
 
 
-zoneIdList.forEach((zoneId, index) => {
-  const date = "2023-02-17T06:33:27.782299";
-  const format = "YYYY/MM/DD HH:mm";
-  const options = {};
-  options.timeZone = zoneId;
-  const result = moment(
-    new Date(
-      new Date(date).toLocaleString("en-US", options)
-    ).toISOString()
-  ).format(format);
-  console.log(`${index + 1}. ${zoneId} ${result}`);
-});
+// zoneIdList.forEach((zoneId, index) => {var
+//   const date = "2023-02-17T06:33:27.782299";
+//   const format = "YYYY/MM/DD HH:mm";
+//   const options = {};
+//   options.timeZone = zoneId;
+//   const result = moment(
+//     new Date(
+//       new Date(date).toLocaleString("en-US", options)
+//     ).toISOString()
+//   ).format(format);
+//   console.log(`${index + 1}. ${zoneId} ${result}`);
+// });
